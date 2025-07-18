7895fa3f3d414ea517864586c6c9132ea21517a0
Iván Dombiak <idombiak@gmail.com>
2025-07-17 20:27:48 -0700

    Added turtwig image and step counter functional in charlie view

--- src/app/src/main/java/edu/chapman/monsutauoka/ui/fourth/CharlieFragment.kt
+++ src/app/src/main/java/edu/chapman/monsutauoka/ui/fourth/CharlieFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import edu.chapman.monsutauoka.MainActivity
 import edu.chapman.monsutauoka.databinding.FragmentCharlieBinding
 import edu.chapman.monsutauoka.extensions.TAG
 import edu.chapman.monsutauoka.ui.fourth.CharlieViewModel
@@ -16,6 +17,17 @@ class CharlieFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: CharlieViewModel by viewModels()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val main = requireActivity() as MainActivity
+        val service = main.getStepCounterService()
+        viewModel.initialize(service)
+    }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,12 +57,14 @@ class CharlieFragment : Fragment() {
         }
 
         // saving previous value through our observer
-        viewModel.text.observe(viewLifecycleOwner) { textValue ->
-            Log.i(TAG, "TextValue: " + textValue + "\n" + "Text Changed: " + binding.editText.text)
+        viewModel.steps.observe(viewLifecycleOwner) { stepValue ->
+            //Log.i(TAG, "TextValue: " + textValue + "\n" + "Text Changed: " + binding.editText.text)
             // text isn't changing correctly, changes the EditText attribute instead of the actual
             // textCharlie attribute? textCharlie doesn't show up unless the viewModel.text has the default value
             // set to it, instead of the correctly updated binding.textCharlie.text = textValue, textValue is always the default text
-            binding.textCharlie.text = textValue.toString()
+
+            // MODIFIED SO THAT STEPS ARE JUST RENDERED
+            binding.textCharlie.text = stepValue.toString()
         }
 
 
--- src/app/src/main/java/edu/chapman/monsutauoka/ui/fourth/CharlieViewModel.kt
+++ src/app/src/main/java/edu/chapman/monsutauoka/ui/fourth/CharlieViewModel.kt
@@ -1,10 +1,27 @@
 package edu.chapman.monsutauoka.ui.fourth
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import edu.chapman.monsutauoka.services.StepCounterService
 
 
 class CharlieViewModel : ViewModel() {
+    private var initialized = false
+
+    private lateinit var _steps: LiveData<Float>
+    val steps: LiveData<Float> get() = _steps
+
+    fun initialize(service: StepCounterService) {
+        if (initialized) {
+            throw IllegalStateException("StepViewModel is already initialized")
+        }
+
+        _steps = service.steps.asLiveData()
+
+        initialized = true
+    }
     // modify from int to string since user will be inputting their string to screen
     // through view model
     val text: MutableLiveData<String> = MutableLiveData<String>("Your Text Will Appear Here!")
