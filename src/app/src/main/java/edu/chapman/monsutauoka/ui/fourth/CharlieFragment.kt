e8319608618381d73a130e9ff6c6f79b198c2a85
ivannnito <idombiak@gmail.com>
2025-07-24 18:00:26 -0700

    Added factory implementation as well as general restructure of charlie fragment

--- src/app/src/main/java/edu/chapman/monsutauoka/ui/fourth/CharlieFragment.kt
+++ src/app/src/main/java/edu/chapman/monsutauoka/ui/fourth/CharlieFragment.kt
@@ -9,24 +9,26 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import edu.chapman.monsutauoka.databinding.FragmentCharlieBinding
 import edu.chapman.monsutauoka.extensions.TAG
+import edu.chapman.monsutauoka.ui.GenericViewModelFactory
+import edu.chapman.monsutauoka.ui.MainFragmentBase
 import edu.chapman.monsutauoka.ui.fourth.CharlieViewModel
 
-class CharlieFragment : Fragment() {
+class CharlieFragment : MainFragmentBase<FragmentCharlieBinding>() {
     private var _binding: FragmentCharlieBinding? = null
-    private val binding get() = _binding!!
 
-    private val viewModel: CharlieViewModel by viewModels()
-
-    override fun onCreateView(
+    override fun createViewBinding(
         inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        Log.d(TAG, ::onCreateView.name)
-
-        _binding = FragmentCharlieBinding.inflate(inflater, container, false)
-        return binding.root
+        container: ViewGroup?
+    ): FragmentCharlieBinding {
+        return FragmentCharlieBinding.inflate(inflater, container, false)
     }
+
+    private val viewModel: CharlieViewModel by viewModels {
+        GenericViewModelFactory {
+            CharlieViewModel(mainActivity.stepCounterService)
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         Log.d(TAG, ::onViewCreated.name)
         // Here we modify our text
@@ -38,6 +40,7 @@ class CharlieFragment : Fragment() {
             Log.i(TAG, "Button Pressed")
 
             viewModel.text.value = binding.editText.text.toString()
+
             // our text value is set to what our user has inputted
             //viewModel.text.value = viewModel.text.toString()
 
@@ -45,14 +48,20 @@ class CharlieFragment : Fragment() {
         }
 
         // saving previous value through our observer
-        viewModel.text.observe(viewLifecycleOwner) { textValue ->
-            Log.i(TAG, "TextValue: " + textValue + "\n" + "Text Changed: " + binding.editText.text)
-            // text isn't changing correctly, changes the EditText attribute instead of the actual
-            // textCharlie attribute? textCharlie doesn't show up unless the viewModel.text has the default value
-            // set to it, instead of the correctly updated binding.textCharlie.text = textValue, textValue is always the default text
-            binding.textCharlie.text = textValue.toString()
-        }
+//        viewModel.text.observe(viewLifecycleOwner) { textValue ->
+//            Log.i(TAG, "TextValue: " + textValue + "\n" + "Text Changed: " + binding.editText.text)
+//            // text isn't changing correctly, changes the EditText attribute instead of the actual
+//            // textCharlie attribute? textCharlie doesn't show up unless the viewModel.text has the default value
+//            // set to it, instead of the correctly updated binding.textCharlie.text = textValue, textValue is always the default text
+//            binding.textCharlie.text = textValue.toString()
+//
+//        }
 
+        viewModel.steps.observe(viewLifecycleOwner) { stepValue ->
+            Log.i(TAG, "CharlieFragment Step Value: " + stepValue)
+            binding.stepCounterText.text = stepValue.toString()
+
+        }
 
     }
 
--- src/app/src/main/java/edu/chapman/monsutauoka/ui/fourth/CharlieViewModel.kt
+++ src/app/src/main/java/edu/chapman/monsutauoka/ui/fourth/CharlieViewModel.kt
@@ -1,11 +1,14 @@
 package edu.chapman.monsutauoka.ui.fourth
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import edu.chapman.monsutauoka.services.StepCounterService
 
-
-class CharlieViewModel : ViewModel() {
+class CharlieViewModel(stepCounterService: StepCounterService) : ViewModel() {
     // modify from int to string since user will be inputting their string to screen
     // through view model
     val text: MutableLiveData<String> = MutableLiveData<String>("Your Text Will Appear Here!")
+    val steps: LiveData<Float> = stepCounterService.steps.asLiveData()
 }
